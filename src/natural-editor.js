f536346e21565836582e6b2efed18b84240a1c65
Alexey Baturin <alexey.baturin.nsk@gmail.com>
2018-04-15 02:17:10 +0700

    Natural editor prototype - help

--- src/natural-editor.js
+++ src/natural-editor.js
@@ -232,6 +232,7 @@ function naturalHeritageEditorMain() {
     }
 
     let listingTemplateName = "natural monument";
+    let helpPageUrl = "https://ru.wikivoyage.org/wiki/%D0%9F%D1%80%D0%B8%D1%80%D0%BE%D0%B4%D0%BD%D1%8B%D0%B5_%D0%BF%D0%B0%D0%BC%D1%8F%D1%82%D0%BD%D0%B8%D0%BA%D0%B8_%D0%A0%D0%BE%D1%81%D1%81%D0%B8%D0%B8";
 
     let listingPageElements = ListingEditorUtils.getListingPageElements();
 
@@ -292,7 +293,9 @@ function naturalHeritageEditorMain() {
                             );
                         },
                         /*onCancel=*/() => alert(2),
-                        /*onHelp=*/() => alert(3)
+                        /*onHelp=*/() => {
+                            window.open(helpPageUrl);
+                        }
                     );
                 }
             );
